d99737442e149de68e1e0eea9fd6f7b80c0b65bc
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2025-05-04 14:21:25 -0400

    Add files via upload
    Updating ShowcaseSection..jsx

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const ShowcaseSection = () => {
     return (
-        <div>ShowcaseSection</div>
+        <div id="work" className="app-showcase">
+            <div className="w-full">
+                <div className="showcaselyout">
+                {/*    LEFT    */}
+                    <div className="first-project-wrapper">
+                        <div className="image-wrapper">
+                        <img src="/images/project1.png" alt="Ryde" />
+                        </div>
+                        <div className="text-content">
+                            <h2>On-Demand Rides Made with a Powerful, User-Friendly App called Ryde</h2>
+                            <p>
+
+                            </p>
+                        </div>
+                    </div>
+
+                {/*    RIGHT    */}
+                </div>
+            </div>
+        </div>
     )
 }
 export default ShowcaseSection
