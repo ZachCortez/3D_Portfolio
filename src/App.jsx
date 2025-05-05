32eb4082f0d2f694efdd97d0d7781550b8988f09
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2025-05-04 22:53:11 -0400

    Add files via upload
    Added to sections folder, Experience.jsx also components folder added files GlowCard.jsx and TitleHeader.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 // import Footer from "./sections/Footer";
 // import Contact from "./sections/Contact";
 // import TechStack from "./sections/TechStack";
-// import Experience from "./sections/Experience";
+import Experience from "./sections/Experience";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 import LogoShowcase from "./sections/LogoShowcase";
@@ -16,7 +16,7 @@ const App = () => (
         <ShowcaseSection />
         <LogoShowcase />
         <FeatureCards />
-        {/*<Experience />*/}
+        <Experience />
         {/*<TechStack />*/}
         {/*<Testimonials />*/}
         {/*<Contact />*/}
